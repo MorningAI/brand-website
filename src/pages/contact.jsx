d2bd9af6dcf51ee15fb0c1100d43c65008def28b
yassercher <yasser.cher42@gmail.com>
2022-04-15 04:39:06 +0100

    first deploy

---
+++ src/pages/contact.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable @next/next/no-sync-scripts */
+import React from "react";
+import Navbar from "../components/Navbar/navbar";
+import Footer from "../components/Footer/footer";
+import DarkTheme from "../layouts/Dark";
+import ContactHeader from "../components/Contact-header/contact-header";
+import ContactForm from "../components/Contact-form/contact-form";
+
+const Contact = () => {
+  const navbarRef = React.useRef(null);
+  const logoRef = React.useRef(null);
+
+  React.useEffect(() => {
+    document.querySelector("body").classList.add("contact-page");
+
+    var navbar = navbarRef.current,
+      logo = logoRef.current;
+    if (window.pageYOffset > 300) {
+      navbar.classList.add("nav-scroll");
+    } else {
+      navbar.classList.remove("nav-scroll");
+    }
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        navbar.classList.add("nav-scroll");
+      } else {
+        navbar.classList.remove("nav-scroll");
+      }
+    });
+    return () => {
+      document.querySelector("body").classList.remove("contact-page");
+    };
+  }, [navbarRef]);
+
+  return (
+    <DarkTheme>
+      <Navbar nr={navbarRef} lr={logoRef} />
+      <ContactHeader />
+      <div className="main-content">
+        <ContactForm />
+        
+
+        <Footer hideBGCOLOR />
+      </div>
+    </DarkTheme>
+  );
+};
+
+export default Contact;
